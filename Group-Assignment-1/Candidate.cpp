9fb11b32364b6d0c9ce36983ce4c1f458213850c
micepram <76100223+micepram@users.noreply.github.com>
2021-11-18 02:05:32 +0530

    Work Chunk #2

--- Group-Assignment-1/Candidate.cpp
+++ Group-Assignment-1/Candidate.cpp
@@ -9,11 +9,16 @@ class Candidate {
 
     // Data Needed to Register A Candidate (Protected) - This data should not need to be changed after object creation.
 
-    int m_candidate_id;                       // College ID - Unique
-    string m_candidate_name;                  // Candidate Name
-    string m_candidate_college_email;
-    char m_candidate_gender;
-    string m_candidate_branch;
+    int m_candidate_id;                       // Candidate's College ID (Unique)
+    string m_candidate_name;                  // Candidate's Name
+    string m_candidate_college_email;         // Candidate's College / University Email
+    char m_candidate_gender;                  // Candidate's Gender
+    string m_candidate_degree;                // Candidate's Degree
+    string m_candidate_branch;                // Candidate's Branch / Specialisation
+    int m_candidate_graduation_year;          // Candidate's Graduation Year
+    int m_candidate_active_backlogs_count;    // Candidate's Active Backlog Count
+    float m_candidate_grade_standard_12;      // Candidate's Aggregate 12th Standard Marks
+    float m_candidate_grade_standard_10;      // Candidate's Aggregate 10th Standard Marks
 
   public:
 
@@ -21,12 +26,21 @@ class Candidate {
 
     string m_candidate_personal_email;
     float m_candidate_cgpa;
-    float m_candidate_min_ctc_preference;
 
     // Data that can be Modified with Time
 
+    float m_candidate_min_ctc_preference;
     bool m_candidate_placed_status;
     string m_candidate_resume_link;
+    string m_candidate_url_portfolio;
+    string m_candidate_url_github;
+    string m_candidate_url_gitlab;
+    string m_candidate_url_linkedin;
+    string m_candidate_url_twitter;
+    string m_candidate_url_hackerrank;
+    string m_candidate_url_codechef;
+    string m_candidate_url_interviewbit;
+    string m_candidate_url_hackerearth;
 
     // Parameterised Default Constructor
 
