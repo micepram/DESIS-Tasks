32060e8d6fbfce1b3445376e33462192a6ee87dc
micepram <76100223+micepram@users.noreply.github.com>
2022-01-04 17:29:47 +0000

    fix

---
+++ Assignment 2 copy/Order.java
@@ -0,0 +1,22 @@
+package JavaAst2;
+
+public class Order {
+
+  String stockName;
+  int price;
+  String tradingParty;
+  boolean direction;
+  String type;
+
+  Order(String stockName, int price, String tradingParty, boolean direction, String type) {
+    this.stockName = stockName;
+    this.price = price;
+    this.tradingParty = tradingParty;
+    this.direction = direction;
+    this.type = type;
+  }
+
+  String toString() {
+    return stockName + " " + price + " " + tradingParty + " " + direction + " " + type;
+  }
+}
